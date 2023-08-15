911716b326f03d5477776b68aa0793bf2923745c
Dawn Smith <dawn.smith@camh.ca>
2023-08-15 11:07:44 -0400

    Fix some bugs, add support for napr's 'all' mode

--- local/napr.sh
+++ local/napr.sh
@@ -3,10 +3,18 @@
 # A version of napr/client/napr.sh that runs fully locally
 
 usage() {
+    echo "Run NAPR locally. Please ensure SUBJECTS_DIR is set before running."
+    echo ""
     echo "Usage: napr.sh [-a] subject_id"
     echo "       -a     Run using all available models. If absent, "
     echo "              only rvmm.full.20161228.model is used."
 }
+
+if [ -z "$SUBJECTS_DIR" ]
+then
+    usage
+    exit
+fi
 
 MODEL=""
 
@@ -25,8 +33,8 @@ done
 SUBJ=$1
 MYDIR=$PWD
 
-LH_THICK=$SUBJ"/surf/lh.thickness.fwhm0.fsaverage4.mgh"
-RH_THICK=$SUBJ"/surf/rh.thickness.fwhm0.fsaverage4.mgh"
+LH_THICK=${SUBJECTS_DIR}/${SUBJ}"/surf/lh.thickness.fwhm0.fsaverage4.mgh"
+RH_THICK=${SUBJECTS_DIR}/${SUBJ}"/surf/rh.thickness.fwhm0.fsaverage4.mgh"
 if [ ! -f $LH_THICK ] || [ ! -f $RH_THICK ] ; then
   echo "File $LH_THICK or $RH_THICK not found."
   usage
--- local/report.age.R
+++ local/report.age.R
@@ -1,16 +1,22 @@
 #!/usr/bin/env Rscript
+
 report.age <- function(lh_file, rh_file, model_type = "") {
     library("kernlab")
     source("../server/napr/R/load.mgh.R")
-    
+
     # Load pre-made models
     load(file="../server/napr/data/rvmm.full.20161228.model.RData")
     if (model_type == "all") {
         load(file="../server/napr/data/gausspr.full.20161228.model.RData")
-        model.list = list(rvmm.full.20161228.model,gausspr.full.20161228.model)
+        model.list = list(
+            rvmm.full.20161228=rvmm.full.20161228.model,
+            gausspr.full.20161228=gausspr.full.20161228.model
+        )
     } else {
-        model.list = list(rvmm.full.20161228.model)
-    }    
+        model.list = list(
+            rvmm.full.20161228=rvmm.full.20161228.model
+        )
+    }
 
     lh <- load.mgh(lh_file)
     rh <- load.mgh(rh_file)
@@ -22,41 +28,11 @@ report.age <- function(lh_file, rh_file, model_type = "") {
     for (i in 1:length(model.list)) {
         model <- model.list[[i]]
         pred.age <- kernlab::predict(model, thick.matrix)
-        model.id <- model.names[i]
+        model.id <- names(model.list)[i]
         cat(sprintf("%-30s %13.1f\n",model.id,pred.age))
     }
-
-    # pred.age <- kernlab::predict(model, thick.matrix)
-    # model.id <- deparse(substitute(model))
-
-    # package.version <- packageVersion("napr")
-
-    # cat(sprintf("%-30s %13s\n","Model","Predicted age"))
-	# cat(sprintf("%-30s %13.1f\n",model.id,pred.age))
 }
 
-
-#!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 
-# report.age <- function(lh_file,rh_file,model = rvmm.full.20161228.model) {
-#     library("kernlab")
-#     source("/opt/quarantine/napr/2023-07-20/src/server/napr/R/load.mgh.R")
-#     load(file="/opt/quarantine/napr/2023-07-20/src/server/napr/data/rvmm.full.20161228.model.RData")
-
-#     lh <- load.mgh(lh_file)
-#     rh <- load.mgh(rh_file)
-
-#         thick.vals <- c(lh$x,rh$x)
-#         thick.matrix <- matrix(data = thick.vals, nrow = 1, ncol = (lh$ndim1 + rh$ndim1))
-
-#     pred.age <- kernlab::predict(model, thick.matrix)
-#     model.id <- deparse(substitute(model))
-
-#     cat(sprintf("%-30s %13s\n","Model","Predicted age"))
-#     cat(sprintf("%-30s %13.1f\n",model.id,pred.age))
-# }
-
-print(args)
-
-report.age(args[1], args[2])
+report.age(args[1], args[2], args[3])
